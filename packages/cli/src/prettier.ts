6c883f8ea6a56803d6460e197badd1a76e508010
alvinirving <alvinirving1@yahoo.com>
2025-03-26 00:33:41 +0000

    Document auth - handling edge cases

--- packages/cli/src/prettier.ts
+++ packages/cli/src/prettier.ts
@@ -1,13 +1,13 @@
 import * as deps from './deps';
 
-const { format: prettierFormat, resolveConfig } = deps.prettier;
-const commonConfig = resolveConfig(process.cwd());
+let { format: prettierFormat, resolveConfig } = deps.prettier;
+let commonConfig = resolveConfig(process.cwd());
 
 export function formatPrettier(
   defaultOptions: Omit<deps.PrettierOptions, 'parser'> &
     Required<Pick<deps.PrettierOptions, 'parser'>>
 ) {
-  const configPromise = commonConfig.then((config) =>
+  let configPromise = commonConfig.then((config) =>
     Object.assign({}, config, defaultOptions)
   );
 
