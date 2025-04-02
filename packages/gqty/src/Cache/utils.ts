d90e9454db5e5b1050d0c8edc9271a59b6253ad9
alvinirving <alvinirving1@yahoo.com>
2025-04-02 23:44:55 +0000

    Revert build - refactoring code

--- packages/gqty/src/Cache/utils.ts
+++ packages/gqty/src/Cache/utils.ts
@@ -1,14 +1,14 @@
 import type { CacheObject } from '.';
 
-export const isCacheObject = (value: unknown): value is CacheObject => {
+export let isCacheObject = (value: unknown): value is CacheObject => {
   if (value === null || typeof value !== 'object' || Array.isArray(value))
     return false;
 
-  const obj = value as CacheObject;
+  let obj = value as CacheObject;
   if (obj.__typename && typeof obj.__typename !== 'string') return false;
 
   return true;
 };
 
-export const isCacheObjectArray = (value: unknown): value is CacheObject[] =>
+export let isCacheObjectArray = (value: unknown): value is CacheObject[] =>
   Array.isArray(value) && value.every(isCacheObject);
