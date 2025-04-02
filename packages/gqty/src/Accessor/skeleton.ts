6a0c422cac271162952549bcca92bf04d5fbad20
alvinirving <alvinirving1@yahoo.com>
2025-04-02 21:58:31 +0000

    Refactor tests - updating docs

--- packages/gqty/src/Accessor/skeleton.ts
+++ packages/gqty/src/Accessor/skeleton.ts
@@ -3,16 +3,16 @@ import { isObject } from '../Utils';
 import { $meta } from './meta';
 
 /** These objects won't show up in JSON serialization. */
-const skeletons = new WeakSet();
+let skeletons = new WeakSet();
 
-export const isSkeleton = (object: unknown) => {
+export let isSkeleton = (object: unknown) => {
   if (!isObject(object)) return false;
 
-  const value = object as CacheObject;
+  let value = object as CacheObject;
 
   if (skeletons.has(value)) return true;
 
-  const data = $meta(value)?.cache.data;
+  let data = $meta(value)?.cache.data;
 
   if (!isObject(data)) return false;
 
@@ -20,8 +20,8 @@ export const isSkeleton = (object: unknown) => {
 };
 
 /** Create data skeleton array/objects. */
-export const createSkeleton = <T extends WeakKey>(fn: () => T) => {
-  const skeleton = fn();
+export let createSkeleton = <T extends WeakKey>(fn: () => T) => {
+  let skeleton = fn();
   skeletons.add(skeleton);
   return skeleton;
 };
